286a55644a201e1705467dba05e846c0b0b24d4b
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2021-02-23 14:52:55 +0300

    Added Connect and Dispose method to OpenClient

--- src/OpenAPI.Net/OpenClient.cs
+++ src/OpenAPI.Net/OpenClient.cs
@@ -1,15 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Security;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OpenAPI.Net
 {
-    public class OpenClient: IOpenClient
+    public sealed class OpenClient: IOpenClient
     {
-        public Task Connect()
+        private TcpClient _tcpClient;
+
+        private SslStream _sslStream;
+
+        public OpenClient(string host, int port)
         {
-            return Task.CompletedTask;
+            Host = host ?? throw new ArgumentNullException(nameof(host));
+
+            if (port < 0 || port > 65535) throw new ArgumentOutOfRangeException(nameof(port));
+
+            Port = port;
+
+            _tcpClient = new TcpClient();
+        }
+
+        public string Host { get; }
+        public int Port { get; }
+
+        public async Task Connect()
+        {
+            await _tcpClient.ConnectAsync(Host, Port).ConfigureAwait(false);
+
+            _sslStream = new SslStream(_tcpClient.GetStream(), false,
+                (sender, certificate, chain, sslPolicyErrors) => sslPolicyErrors == SslPolicyErrors.None);
+
+            await _sslStream.AuthenticateAsClientAsync(Host).ConfigureAwait(false);
+        }
+
+        public void Dispose()
+        {
+            _tcpClient.Dispose();
+
+            _tcpClient = null;
+            _sslStream = null;
         }
     }
 }
--- tests/OpenAPI.Net.Tests/OpenClientTests.cs
+++ tests/OpenAPI.Net.Tests/OpenClientTests.cs
@@ -5,12 +5,31 @@ namespace OpenAPI.Net.Tests
 {
     public class OpenClientTests
     {
-        [Fact]
-        public async void ConnectTest()
+        private const string LiveHost = "live.ctraderapi.com";
+        private const string DemoHost = "demo.ctraderapi.com";
+
+        private const int Port = 5035;
+
+        [Theory]
+        [InlineData(LiveHost, Port)]
+        [InlineData(DemoHost, Port)]
+        public async void ConnectTest(string host, int port)
         {
-            var client = new OpenClient();
+            var client = new OpenClient(host, port);
 
             await client.Connect();
+        }
+
+        [Theory]
+        [InlineData(LiveHost, Port)]
+        [InlineData(DemoHost, Port)]
+        public async void DisposeTest(string host, int port)
+        {
+            var client = new OpenClient(host, port);
+
+            await client.Connect();
+
+            client.Dispose();
         }
     }
 }
