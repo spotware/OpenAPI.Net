be3f5a0581caa99790772a46a413ab61802e2dce
Ahmad Noman Musleh <8859902+afhacker@users.noreply.github.com>
2021-06-29 20:07:57 +0300

    Added daily to chart legend

--- src/ASP.NET.Demo/wwwroot/js/trading-accounts.js
+++ src/ASP.NET.Demo/wwwroot/js/trading-accounts.js
@@ -514,6 +514,12 @@
 
         tradingAccountConnection.invoke("StopErrors", previousAccountLogin).catch(onError);
 
+        $('#historyTableBody').html('');
+        $('#transactionsTableBody').html('');
+        $('#symbolsTableBody').html('');
+        $('#positions-table-body').html('');
+        $('#orders-table-body').html('');
+
         var accountLogin = $("#accounts-list").val();
 
         $("#accounts-list").data("previousAccountLogin", accountLogin);
@@ -728,7 +734,7 @@
     function createSymbolChart(symbolId) {
         var accountLogin = parseInt($("#accounts-list").val());
 
-        tradingAccountConnection.invoke('getSymbolTrendbars', accountLogin, symbolId).then(data => createChart(data.name, data.ohlc)).catch(onError);
+        tradingAccountConnection.invoke('getSymbolTrendbars', accountLogin, symbolId).then(data => createChart(`${data.name} Daily`, data.ohlc)).catch(onError);
     }
 
     var ctx = document.getElementById('chartCanvas').getContext('2d');
@@ -772,37 +778,4 @@
 
         chart.update();
     }
-
-    function randomNumber(min, max) {
-        return Math.random() * (max - min) + min;
-    }
-
-    function randomBar(date, lastClose) {
-        var open = randomNumber(lastClose * 0.95, lastClose * 1.05).toFixed(2);
-        var close = randomNumber(open * 0.95, open * 1.05).toFixed(2);
-        var high = randomNumber(Math.max(open, close), Math.max(open, close) * 1.1).toFixed(2);
-        var low = randomNumber(Math.min(open, close) * 0.9, Math.min(open, close)).toFixed(2);
-
-        return {
-            t: date.valueOf(),
-            o: open,
-            h: high,
-            l: low,
-            c: close
-        };
-    }
-
-    function getRandomData(count) {
-        var date = luxon.DateTime.fromRFC2822('01 Apr 2017 00:00 Z');
-        var data = [randomBar(date, 30)];
-        while (data.length < count) {
-            date = date.plus({ days: 1 });
-            if (date.weekday <= 5) {
-                data.push(randomBar(date, data[data.length - 1].c));
-            }
-        }
-        return data;
-    }
-
-    //createChart("Test", getRandomData(100));
 });
