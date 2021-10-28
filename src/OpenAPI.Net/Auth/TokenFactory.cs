10dc12372a9ff647c91d124b291d47c70ac5d43a
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2021-10-28 09:52:42 +0300

    Added docs snippet for TokenFactory GetToken method

--- src/OpenAPI.Net/Auth/TokenFactory.cs
+++ src/OpenAPI.Net/Auth/TokenFactory.cs
@@ -9,9 +9,56 @@ namespace OpenAPI.Net.Auth
 {
     public static class TokenFactory
     {
+        /// <summary>
+        /// This method returns an API access token
+        /// It creates an instance of HttpClient and then disposes that instance after sending the request
+        /// If you are using HttpClientFactory then user the other overload of GetToken method that you can pass an HttpClient
+        /// </summary>
+        /// <param name="authCode">Authentication code</param>
+        /// <param name="app">API application</param>
+        /// <param name="authUri">Optional, if you don't pass a value for this parameter it will use the ApiInfo.AuthUrl</param>
+        /// <exception cref="ArgumentNullException">When any of the method parameters were null.</exception>
+        /// <exception cref="HttpRequestException">If HttpRequest failed</exception>
+        /// <returns>Token</returns>
         public static async Task<Token> GetToken(string authCode, App app, string authUri = ApiInfo.AuthUrl)
         {
             using var client = new HttpClient();
+
+            return await GetToken(authCode, app, client, authUri);
+        }
+
+        /// <summary>
+        /// This method returns an API access token
+        /// Use this overload of GetToken method if you have an HttpClientFactory
+        /// </summary>
+        /// <param name="authCode">Authentication code</param>
+        /// <param name="app">API application</param>
+        /// <param name="client">The HttpClient instance that will be used for sending request</param>
+        /// <param name="authUri">Optional, if you don't pass a value for this parameter it will use the ApiInfo.AuthUrl</param>
+        /// <exception cref="ArgumentNullException">When any of the method parameters were null.</exception>
+        /// <exception cref="HttpRequestException">If HttpRequest failed</exception>
+        /// <returns>Token</returns>
+        public static async Task<Token> GetToken(string authCode, App app, HttpClient client, string authUri = ApiInfo.AuthUrl)
+        {
+            if (string.IsNullOrEmpty(authCode))
+            {
+                throw new ArgumentException($"'{nameof(authCode)}' cannot be null or empty.", nameof(authCode));
+            }
+
+            if (app is null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            if (client is null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+
+            if (authUri is null)
+            {
+                throw new ArgumentNullException(nameof(authUri));
+            }
 
             var query = new NameValueCollection
             {
