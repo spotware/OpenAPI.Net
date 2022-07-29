1050783afb215546ae57589ef33b30e17b2e9f92
vesofx <vesofx@zohomail.eu>
2022-07-29 23:24:22 +0300

    Events

---
+++ src/OpenAPI.Net/Events/EventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAPI.Net.EventsArgs
+{
+    public class TrailingSlChangedEventArgs : EventArgs { public ProtoOATrailingSLChangedEvent Message { get; set; } }
+    public class SymbolChangedEventArgs : EventArgs { public ProtoOASymbolChangedEvent Message { get; set; } }
+    public class TraderUpdateEventArgs : EventArgs { public ProtoOATraderUpdatedEvent Message { get; set; } }
+    public class ExecutionEventArgs : EventArgs { public ProtoOAExecutionEvent Message { get; set; } }
+    public class SpotEventArgs : EventArgs { public ProtoOASpotEvent Message { get; set; } }
+    public class OrderErrorEventArgs : EventArgs { public ProtoOAOrderErrorEvent Message { get; set; } }
+    public class MarginChangedEventArgs : EventArgs { public ProtoOAMarginChangedEvent Message { get; set; } }
+    public class AccountsTokenInvalidatedEventArgs : EventArgs { public ProtoOAAccountsTokenInvalidatedEvent Message { get; set; } }
+    public class ClientDisconnectEventArgs : EventArgs { public ProtoOAClientDisconnectEvent Message { get; set; } }
+    public class DepthEventArgs : EventArgs { public ProtoOADepthEvent Message { get; set; } }
+    public class AccountDisconnectEventArgs : EventArgs { public ProtoOAAccountDisconnectEvent Message { get; set; } }
+    public class MarginCallUpdateEventArgs : EventArgs { public ProtoOAMarginCallUpdateEvent Message { get; set; } }
+    public class MarginCallTriggerEventArgs : EventArgs { public ProtoOAMarginCallTriggerEvent Message { get; set; } }
+}
---
+++ src/OpenAPI.Net/SimpleAPI.Events.cs
@@ -0,0 +1,90 @@
+﻿using Google.Protobuf;
+using OpenAPI.Net.EventsArgs;
+using System;
+using System.Threading.Tasks;
+
+namespace OpenAPI.Net
+{
+    public sealed partial class OpenClient
+    {
+        public delegate Task EventHandlerAsnyc<TEventArgs>(object sender, TEventArgs eventArgs);
+
+        public event EventHandlerAsnyc<TrailingSlChangedEventArgs> TrailingSlChangedEvent;
+        public event EventHandlerAsnyc<SymbolChangedEventArgs> SymbolChangedEvent;
+        public event EventHandlerAsnyc<TraderUpdateEventArgs> TraderUpdateEvent;
+        public event EventHandlerAsnyc<ExecutionEventArgs> ExecutionEvent;
+        public event EventHandlerAsnyc<SpotEventArgs> SpotEvent;
+        public event EventHandlerAsnyc<OrderErrorEventArgs> OrderErrorEvent;
+        public event EventHandlerAsnyc<MarginChangedEventArgs> MarginChangedEvent;
+        public event EventHandlerAsnyc<AccountsTokenInvalidatedEventArgs> AccountsTokenInvalidatedEvent;
+        public event EventHandlerAsnyc<ClientDisconnectEventArgs> ClientDisconnectEvent;
+        public event EventHandlerAsnyc<DepthEventArgs> DepthEvent;
+        public event EventHandlerAsnyc<AccountDisconnectEventArgs> AccountDisconnectEvent;
+        public event EventHandlerAsnyc<MarginCallUpdateEventArgs> MarginCallUpdateEvent;
+        public event EventHandlerAsnyc<MarginCallTriggerEventArgs> MarginCallTriggerEvent;
+
+        public async Task Raise<TEventArgs>(EventHandlerAsnyc<TEventArgs> handlers, TEventArgs eventArgs)
+        {
+            if (handlers == null)
+                return;
+
+            //foreach (var subscriber in handlers.GetInvocationList())
+            //    await ((EventHandlerAsnyc<TEventArgs>)subscriber).Invoke(sender, eventArgs);
+            //Delegate[] subscribers = handlers.GetInvocationList();
+            //await Task.WhenAll(subscribers.Select(s => ((EventHandlerAsnyc<TEventArgs>)s).Invoke(this, eventArgs)));
+            lock (this)
+            {
+                Delegate[] subscribers = handlers.GetInvocationList();
+                Parallel.ForEach(subscribers, s => ((EventHandlerAsnyc<TEventArgs>)s).Invoke(this, eventArgs));
+            }
+        }
+
+        private async Task MessageEventHandlersAsync(IMessage message)
+        {
+
+            switch (message)
+            {
+                case ProtoOASpotEvent pOASpotEvent:
+                    await Raise(SpotEvent, new SpotEventArgs { Message = pOASpotEvent });
+                    break;
+                case ProtoOAExecutionEvent pOAExecutionEvent:
+                    await Raise(ExecutionEvent, new ExecutionEventArgs { Message = pOAExecutionEvent });
+                    break;
+                case ProtoOATrailingSLChangedEvent pOATrailingSlChangedEvent:
+                    await Raise(TrailingSlChangedEvent, new TrailingSlChangedEventArgs { Message = pOATrailingSlChangedEvent });
+                    break;
+                case ProtoOASymbolChangedEvent pOASymbolChangedEvent:
+                    await Raise(SymbolChangedEvent, new SymbolChangedEventArgs { Message = pOASymbolChangedEvent });
+                    break;
+                case ProtoOATraderUpdatedEvent pOATraderUpdateEvent:
+                    await Raise(TraderUpdateEvent, new TraderUpdateEventArgs { Message = pOATraderUpdateEvent });
+                    break;
+                case ProtoOAOrderErrorEvent pOAOrderErrorEvent:
+                    await Raise(OrderErrorEvent, new OrderErrorEventArgs { Message = pOAOrderErrorEvent });
+                    break;
+                case ProtoOAMarginChangedEvent pOAMarginChangedEvent:
+                    await Raise(MarginChangedEvent, new MarginChangedEventArgs { Message = pOAMarginChangedEvent });
+                    break;
+                case ProtoOAAccountsTokenInvalidatedEvent pOAAccountsTokenInvalidatedEvent:
+                    await Raise(AccountsTokenInvalidatedEvent, new AccountsTokenInvalidatedEventArgs { Message = pOAAccountsTokenInvalidatedEvent });
+                    break;
+                case ProtoOAClientDisconnectEvent pOAClientDisconnectEvent:
+                    await Raise(ClientDisconnectEvent, new ClientDisconnectEventArgs { Message = pOAClientDisconnectEvent });
+                    break;
+                case ProtoOADepthEvent pOADepthEvent:
+                    await Raise(DepthEvent, new DepthEventArgs { Message = pOADepthEvent });
+                    break;
+                case ProtoOAAccountDisconnectEvent pOAAccountDisconnectEvent:
+                    await Raise(AccountDisconnectEvent, new AccountDisconnectEventArgs { Message = pOAAccountDisconnectEvent });
+                    break;
+                case ProtoOAMarginCallUpdateEvent pOAMarginCallUpdateEvent:
+                    await Raise(MarginCallUpdateEvent, new MarginCallUpdateEventArgs { Message = pOAMarginCallUpdateEvent });
+                    break;
+                case ProtoOAMarginCallTriggerEvent pOAMarginCallTriggerEvent:
+                    await Raise(MarginCallTriggerEvent, new MarginCallTriggerEventArgs { Message = pOAMarginCallTriggerEvent });
+                    break;
+
+            }
+        }
+    }
+}
